8b5296fc9b56c5f3adae11ec00bae25a887492da
SebasOrtiz <sebastian.santacoloma@agriculturaverion.ind.br>
2024-05-04 23:09:33 -0300

    Import N Export

---
+++ 0javascriptrefresher/assets/scripts/app.js
@@ -0,0 +1,7 @@
+﻿import apiKey from "./util.js";
+import {apiKey1, apiKey2 as content} from "./util.js";
+import * as utils from "./util.js";
+
+console.log(apiKey);
+console.log(utils.apiKey1);
+console.log(content);
