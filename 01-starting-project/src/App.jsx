f0bd9556110d32ecaafc6de5fb59c7ce3fa02d3a
SebasOrtiz <sebastian.santacoloma@agriculturaverion.ind.br>
2024-06-30 21:58:37 -0300

    Sebas: [add] Using dynamic values

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,18 @@
+import reactImg from './assets/react-core-concepts.png'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+function genRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 function Header() {
+    let description = reactDescriptions[genRandomInt(reactDescriptions.length)];
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+            <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you are
+                {description} React concepts you will need for almost any app you are
                 going to build!
             </p>
         </header>
