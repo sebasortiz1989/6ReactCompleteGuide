38b7fb6e42462c97592e1ea04a16d3fc86e2a07a
SebasOrtiz <sebastian.santacoloma@agriculturaverion.ind.br>
2024-07-01 21:47:49 -0300

    Sebas: [add] Creating and using a first custom component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS } from "./data.js";
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 function genRandomInt(max) {
@@ -19,11 +21,33 @@ function Header() {
     );
 }
 
+function CoreConcept({image, title, description}) {
+    return (
+        <li>
+            <img src={image} alt={title}/>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    )
+}
+
 function App() {
     return (
         <div>
             <Header/>
             <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept {...CORE_CONCEPTS[0]}/>
+                        <CoreConcept {...CORE_CONCEPTS[1]}/>
+                        <CoreConcept {...CORE_CONCEPTS[2]}/>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[3].title}
+                            description={CORE_CONCEPTS[3].description}
+                            image={CORE_CONCEPTS[3].image}/>
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>
